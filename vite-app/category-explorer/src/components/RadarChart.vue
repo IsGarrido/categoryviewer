957cf4026afdf3dcf9b032da3d76a9452b0d152c
ismaelgarridomunoz@gmail.com <Ismael Garrido Muoz>
2022-03-27 19:35:34 +0200

    App migrada a vite

---
+++ vite-app/category-explorer/src/components/RadarChart.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <canvas id="chart"></canvas>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+
+//import Chart from 'chart.js'
+import Chart from "chart.js/auto";
+
+export default {
+  props: ["labels", "datam", "dataf", "type"],
+  mounted() {
+    const planetChartData = {
+      type: this.type || "radar",
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Masculino",
+            data: this.datam,
+            backgroundColor: "rgba(102, 204, 255,.5)",
+            borderColor: "#000000",
+            borderWidth: 2,
+          },
+          {
+            label: "Femenino",
+            data: this.dataf,
+            backgroundColor: "rgba(255, 153, 255,.5)",
+            borderColor: "#000000",
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        lineTension: 0,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                padding: 25,
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    const ctx = document.getElementById("chart");
+    new Chart(ctx, planetChartData);
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
